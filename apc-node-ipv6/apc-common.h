37dc967f5d1fc09dfba6aea1b9385ff1f87efda6
Luis Mercado <luisbzmercado1776@gmail.com>
2020-01-11 14:01:20 +0800

    MQTT feature added
    -set sink as an mqtt publisher (sink will hold all data of sensor nodes and publish in regular intervals)
    -rpl border router implemented (copied from examples in contiki)
    -part of border router implementation is the prefix inheritance of the apc nodes, they will now share the same prefix as the border router.
    -standardized addressing configuration across apc nodes

--- apc-node-ipv6/apc-common.h
+++ apc-node-ipv6/apc-common.h
@@ -1,3 +1,16 @@
+#ifndef APC_COMMON_H
+#define APC_COMMON_H
+/*---------------------------------------------------------*/
+#include "contiki.h"
+#include "contiki-net.h"
+#include "net/ip/uip.h"
+/*---------------------------------------------------------*/
+/* This defines the maximum amount of sensor nodes we can remember. */
+#define MAX_SENSOR_NODES 3
+/*---------------------------------------------------------*/
+#define LOCAL_ADDR_PRINT_INTERVAL 30
+#define PREFIX_UPDATE_INTERVAL 20
+/*---------------------------------------------------------*/
 //these are the types of data that can be communicated between sensor nodes and the sink node
 enum
 {
@@ -5,6 +18,7 @@ enum
 	COLLECTOR_ADV, //advertise as collector mote
 	COLLECTOR_ACK, //acknowledge as collector mote
 	SINK_ADV, //advertise as sink mote
+	SINK_ACK, //acknowledge as sink mote
 	DATA_REQUEST,
 	DATA_ACK,
 	
@@ -22,14 +36,50 @@ enum
 	WIND_SPEED_T, //unit in m/s
 	WIND_DRCTN_T //may be N, S, E, W and their combinations
 };
-
+/*---------------------------------------------------------*/
 typedef struct {
 	uint32_t seq;
 	uint8_t type;
 	char data[8];
 } node_data_t;
-
+/*---------------------------------------------------------*/
 typedef struct {
 	uint8_t type;
 	char data[8];
 } sensor_reading_t;
+/*---------------------------------------------------------*/
+//REF: example-neighbors.c in contiki examples
+/* This structure holds information about sensor nodes. */
+typedef struct sensor_node {
+	/* The ->next pointer is needed since we are placing these on a
+		Contiki list. */
+	struct sensor_node *next;
+
+	/* The ->addr field holds the IPv6 address of the sensor node. */
+	uip_ipaddr_t addr;
+
+	/* Sensor readings data for a node */
+	char temperature[8]; //unit in Deg. Celsius
+	char humidity[8];    //unit in %RH (0%-100%)
+	char PM25[8];        //unit in ppm, compute to microgram per m3 at processing server
+	char CO[8];          //unitless (ratio in milli scale), compute to ppm at processing server
+	char CO2[8];         //unitless (ratio in milli scale), compute to ppm at processing server
+	char O3[8];          //unitless (ratio in milli scale), compute to ppm at processing server
+	char windSpeed[8];   //unit in m/s
+	char windDir[2];     //may be N, S, E, W and their combinations (NW, NE, SW, SE)
+} sensor_node_t;
+/*---------------------------------------------------------*/
+void
+print_local_addresses(void* data);
+/*---------------------------------------------------------*/
+void
+set_local_ip_addresses(uip_ipaddr_t* prefix_64, uip_ipaddr_t* local_addr);
+/*---------------------------------------------------------*/
+void
+set_remote_ip_addresses(uip_ipaddr_t* prefix_64, uip_ipaddr_t* remote_addr);
+/*---------------------------------------------------------*/
+void
+update_ip_addresses_prefix(void* prefix_64);
+/*---------------------------------------------------------*/
+#endif
+/*---------------------------------------------------------*/
