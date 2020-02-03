a30005652b2f74ae769773aebab968b4c6a228e4
Luis Mercado <luisbzmercado1776@gmail.com>
2020-02-03 23:54:30 +0800

    added weather api getter for mqtt subscriber
    integrated weather condition info to mqtt published info

---
+++ apc-node-ipv6/mqtt-subscriber/weather_access.py
@@ -0,0 +1,39 @@
+import configparser
+import requests
+import json
+import sys
+
+def get_api_key():
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+    return config['openweathermap']['api_key']
+ 
+def get_weather(api_key, location):
+    url = "https://api.openweathermap.org/data/2.5/weather?q={}&units=metric&appid={}".format(location, api_key)
+    r = requests.get(url)
+    if r.status_code == 404:
+        print("Weather API: ERROR - API url does not exist.")
+        return None
+    elif r.status_code == 403:
+        print("Weather API: ERROR - Unauthorized.")
+        return None
+    else:
+        return r.json()['weather']
+        
+def main():
+    if len(sys.argv) != 2:
+        exit("Usage: {} LOCATION".format(sys.argv[0]))
+    location = sys.argv[1]
+    record = {}
+    api_key = get_api_key()
+    weather = get_weather(api_key, location)
+    if weather is not None:
+        weatherList = []
+        for weatherInfo in weather:
+            weatherList.append(weatherInfo['main'])
+        record['weather'] = weatherList;
+ 
+    print(record)
+    
+if __name__=="__main__":
+    main()
