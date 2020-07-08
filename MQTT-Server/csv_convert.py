7b29fefad30880a55d5ba7b12c5a1be847688505
LBMercado <luisbzmercado1776@gmail.com>
2020-07-08 00:24:24 -0700

    Add csv convertion python script
    - Add script to make use of the csv conversion functionality defined in
    to_csv_converter.py

---
+++ MQTT-Server/csv_convert.py
@@ -0,0 +1,29 @@
+from data_access import DataAccess
+from to_csv_converter import CsvConverter
+
+''' DB Configuration '''
+DB_ADDRESS = 'localhost'
+DB_PORT = 27017
+DB_NAME = 'apc-iot'
+DB_COLL_NAME = 'apc_data'
+''' CSV Configuration '''
+CSV_FILENAME = 'air_quality_data.csv'
+
+def main():
+    db_access = DataAccess(DB_ADDRESS, DB_PORT, DB_COLL_NAME)
+    db_access.connect_db(DB_NAME)
+
+    columns_sensor = ['Temperature (°C)', 'Humidity (%RH)', 'PM25 (ug/m3)',
+                      'CO (PPM)', 'NO2 (PPM)', 'O3 (PPB)',
+                      'Wind Speed (m/s)', 'Wind Direction']
+    columns_external = ['weather', 'date']
+    invalid_values = ['', 0, -1]
+
+    converter = CsvConverter(db_access, columns_sensor, columns_external, invalid_values,
+                             CSV_FILENAME)
+
+    converter.convert()
+
+
+if __name__ == '__main__':
+    main()
